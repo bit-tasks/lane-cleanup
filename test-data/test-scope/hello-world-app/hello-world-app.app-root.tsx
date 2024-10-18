8e89a179bfd8507c38e64bdf0a8e0b7860751692
ashanfernando <ashan256@gmail.com>
2024-10-18 10:59:05 +0530

    Remove app from test

--- test-data/test-scope/hello-world-app/hello-world-app.app-root.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import { HelloWorld } from '@learnbit/hello-world.ui.hello-world';
-
-export const HelloWorldApp = () => <HelloWorld />;
-
-const root = document!.getElementById('root');
-ReactDOM.createRoot(root as HTMLElement).render(<HelloWorldApp />);
